6e9e51bae3fae788187787bc24f358ce8b394491
idekkusu <154049838+idekkusu@users.noreply.github.com>
2023-12-25 20:35:32 +0800

    Update CA2 editing acutal submitting copy.py
    Parts 3 and 4 finished

--- CA2 editing acutal submitting copy.py
+++ CA2 editing acutal submitting copy.py
@@ -91,9 +91,6 @@ elif int(option) == 2:
 elif int(option) == 3:
 '''
 
-#Importing Modules
-
-
 
 #Initialising Variables
 option = ""
@@ -102,17 +99,20 @@ exit = False
 
 input_string = "Select an option: "
 
-list=[["No","Name", "Capitalization", "QtyBought", "Bought Price", "Current Price"],
-[1, "Bitcoin", "High", 15, 38000, 62000],
-[2, "Ethereum", "High", 90, 4200, 3500],
-[3, "Solana", "Mid", 60, 260, 110],
-[4, "Decentraland", "Mid", 30000, 1.5, 5],
-[5, "The Sandbox", "Mid", 25000, 2, 4],
-[6, "Dogecoin", "Low", 55000, 0.4, 0.15]]
+list=[["Name", "Capitalization", "QtyBought", "Bought Price", "Current Price"],
+["Bitcoin", "High", 15, 38000, 62000],
+["Ethereum", "High", 90, 4200, 3500],
+["Solana", "Mid", 60, 260, 110],
+["Decentraland", "Mid", 30000, 1.5, 5],
+["The Sandbox", "Mid", 25000, 2, 4],
+["Dogecoin", "Low", 55000, 0.4, 0.15]]
 
 list2 = ["Index","Name","Market Cap","Quantity Bought","Buy In Price","Market Price","E. Edit Complete. Exit"]
 
-#Menu Display
+
+
+
+#Main Menu Display
 def Menu():
     print("-"*80)
     print("\tClass 02\n \t1. Jeeva \n \t2. Edison") 
@@ -138,7 +138,12 @@ def DisplayCrypto():
         #print(str(list[list.index(row)][0])+str(space(5-len(str(list[list.index(row)][0])))),str(list[list.index(row)][1])+str(space(15-len(str(list[list.index(row)][1])))), str(list[list.index(row)][2])+str(space(15-len(str(list[list.index(row)][2])))), str(list[list.index(row)][3])+str(space(15-len(str(list[list.index(row)][3])))), str(list[list.index(row)][4])+str(space(15-len(str(list[list.index(row)][4])))), str(list[list.index(row)][5])+str(space(9-len(str(list[list.index(row)][0])))))
         for col in row:
             if row.index(col)== 0:
-                printedrow += str(col).ljust(7)
+                    if list.index(row) == 0:
+                        text = "No"
+                        printedrow += text.ljust(7)
+                    else:
+                        printedrow += str(list.index(row)).ljust(7)
+                        printedrow += str(col).ljust(17)
             else:
                 printedrow += str(col).ljust(17)
         print(printedrow)
@@ -184,7 +189,7 @@ def AmendCrypto():
     for i in range(len(list)):
         if i == 0:
             continue
-        print(i ,"-", list[i][1])
+        print(i ,"-", list[i][0])
     input_string = "Enter 1 to " + str(len(list)-1) +  " for your selection or 'E' to exit: "
     print('-'*80)
     while True:
@@ -193,8 +198,106 @@ def AmendCrypto():
             if int(x) < 1 or int(x)>len(list)-1: #checking for out of range
                 input_string = "Please only input 1 to " + str(len(list)-1) +  " for your selection or 'E' to exit: "
                 continue
-            else:
-                break
+            else: #If input is valid
+                    for i in range(len(list[int(x)])):
+                        if i != 0:
+                            print(str(i) +'.',list2[i].ljust(25)+':',list[int(x)][i])
+                        else:
+                            print()
+                            print(list2[i].ljust(25)+'   :',list[int(x)][i])
+                    print(list2[6])
+                    
+                    input_string2 = "What do you want to edit?: "
+                    while True:
+                        y = input(input_string2)
+
+                        if y.isnumeric(): #Checking if input is numeric
+                            if int(y) < 1 or int(y)>len(list2)-2: #checking for out of range
+                                input_string2 = "Please only input 1 to " + str(len(list2)-2) +  " for your selection or 'E' to exit: "
+                                continue
+                            else: #if y is a valid input and is not "E"
+                                match y:
+                                    case "1":
+                                        input_string3 = "(1) Enter new Name of Crypto: "
+                                        while True:
+                                            z = input(input_string3)
+                                            for i in range(len(list)):
+                                                if z.title() == list[i][1]:
+                                                    print(list[i][1], "already exists, please enter a new cryptocurrency")
+                                                    input_string3 = str(list[i][1]) + " already exists, please enter a new cryptocurrency"
+                                                    continue
+                                            list[int(x)][int(y)] = z
+                                            input("Amendment Successful! Press Enter to Continue")
+                                            break
+
+                                    case "2":
+                                        input_string3 = "(2) Enter new Market Cap of Crypto: "
+                                        while True:
+                                            z = input(input_string3)
+                                            caselist=("high","low","mid")#using this ignores the bracket nonsense
+                                            if z.lower() not in caselist:
+                                                input_string2 = "Please key in a valid edit (High, Low, Mid)"
+                                                continue
+                                            z = z.capitalize()
+                                            break
+                                        list[int(x)][int(y)] = z
+                                        input("Amendment Successful! Press Enter to Continue")
+                                        break
+                                        
+                                    case "3":
+                                        input_string3 = "(3) Enter new Quantity of Crypto Bought = "
+                                        while True:
+                                            z = input(input_string3)
+                                            try:
+                                                z = int(z)
+                                            except ValueError :
+                                                input_string3 = "Please key in a valid input: "
+                                                continue
+                                            else:
+                                                list[int(x)][int(y)] = z
+                                                input("Amendment Successful! Press Enter to Continue")
+                                                break
+
+                                    case "4":
+                                        input_string3 = "(4) Enter new Buy In Price of Crypto = "
+                                        while True:
+                                            z = input(input_string3)
+                                            try:
+                                                z = float(z)
+                                            except ValueError :
+                                                input_string3 = "Please key in a valid input: "
+                                                continue
+                                            else:
+                                                list[int(x)][int(y)] = z
+                                                input("Amendment Successful! Press Enter to Continue")
+                                                break
+                                    case "5": 
+                                        input_string3 = "(5) Enter new Market Price of Crypto = "
+                                        while True:
+                                            z = input(input_string3)
+                                            try:
+                                                z = float(z)
+                                            except ValueError :
+                                                input_string3 = "Please key in a valid input: "
+                                                continue
+                                            else:
+                                                list[int(x)][int(y)] = z
+                                                input("Amendment Successful! Press Enter to Continue")
+                                                break
+                                                
+                                    case _:
+                                        input_string2 = "Please input a valid option: "
+                                        continue
+                                break
+
+                        elif y.isalpha():
+                            if y.upper() == 'E': #check for E
+                                break
+                            else: #check for other alphabetical input that is not E
+                                input_string2 = "Please only input 1 to " + str(len(list2)-2) +  " for your selection or 'E' to exit: "
+                                continue
+                    break
+
         elif x.isalpha():
             if x.upper() == 'E': #check for E
                 break
@@ -202,34 +305,33 @@ def AmendCrypto():
                 input_string = "Please only input 1 to " + str(len(list)-1) +  " for your selection or 'E' to exit: "
                 continue
       
-            
-    for i in range(len(list[int(x)])):
-        print(str(i) +'.',list2[i].ljust(25)+':',list[int(x)][i])
-    print(list2[6])
-    
-    input_string2 = "What do you want to edit?: "
-    while True:
-        y = input(input_string2)
-        if y.isnumeric(): #Checking if input is numeric
-            if int(y) < 1 or int(y)>len(list2)-2: #checking for out of range
-                input_string2 = "Please only input 1 to " + str(len(list2)-2) +  " for your selection or 'E' to exit: "
-                continue
-            else:
-                break
-        elif y.isalpha():
-            if y.upper() == 'E': #check for E
-                break
-            else: #check for other alphabetical input that is not E
-                input_string2 = "Please only input 1 to " + str(len(list2)-2) +  " for your selection or 'E' to exit: "
-                continue
-    input_string3 = '('+y+') '+'Enter new '+ list2[int(y)]+ ' of Crypto:'
-    list[int(x)][int(y)] = input(input_string3)
-    input("Amendment Successful! Press Enter to Continue")
-
+        
 
 #Option 4: Remove CryptoCurrency
 def RemoveCrypto():
-    print('why')
+    print('No - CryptoCurrency')
+    print('-'*80)
+    for i in range(len(list)):
+        if i == 0:
+            continue
+        print(i ,"-", list[i][0])
+    print('-'*80)
+    input_string = "Enter 1 to " + str(len(list)-1) +  " for your selection or 'E' to exit: "
+    while True:
+        x = input(input_string)
+        if x.isnumeric() and (int(x) < 1 or int(x)>len(list)-1): #Checking if input is within numeric options
+            input_string = "Please only input 1 to " + str(len(list)-1) +  " for your selection or 'E' to exit: "
+            continue
+        elif x.isalpha() and x.upper()!= "E": # checking for other inputs
+            input_string = "Please key in a valid input: "
+            continue
+        elif x.upper() == "E": # checking for E
+            break
+        else: #valid option
+            list.pop(int(x))
+            input("Your Choice has been Successfully Removed! Press Enter to Continue")
+            break
+
 
 #Option 5: Crypto Portfolio Statement
 def CryptoPortfolioStatement():
@@ -244,6 +346,9 @@ def Student2():
     print('why')
 
 
+
+#MAIN PROGRAMME
+    
 while True:
     Menu()
     option = input(input_string)
@@ -251,15 +356,12 @@ while True:
     match option:
         case "1":
             DisplayCrypto()
-
         case "2":
             AddCrypto()
-            
         case "3":
             AmendCrypto()
-
         case "4":
-            break
+            RemoveCrypto()
         case "5":
             break
         case "6":
